d18c5d429a27124cde35909f6ec280ff6a46265c
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2022-03-30 07:45:49 -0600

    Modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,17 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
 
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:marianamtzcelis/labdeweb_mascotas.git master:gh-pages
+git push -f git@github.com:marianamtzcelis/labdeweb_mascotas.git main:gh-pages
 
 cd -
