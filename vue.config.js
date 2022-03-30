3c30f022528e4eb3dc8c082771ef3cf32121dee0
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2022-03-29 22:43:39 -0600

    Changed vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/labdeweb_mascotas/'
-    : '/'
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/labdeweb_mascotas/" : "/",
   transpileDependencies: true
 })
